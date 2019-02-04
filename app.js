ced4be63d38bfd756233acda050cc933137b9461
verzion2k <kovacs.zsoltzx@gmail.com>
2019-02-04 15:33:56 +0100

    Skeleton updated

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.listen(3000, function(req, res) {
+    console.log("Runnin'...")
+});
