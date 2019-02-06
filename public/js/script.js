f4af1c16a9ce4c1c74e1f6a3d06b43a66cd7af7b
verzion2k <kovacs.zsoltzx@gmail.com>
2019-02-06 19:01:45 +0100

    Finished the basic site

--- public/js/script.js
+++ public/js/script.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
         $(this).toggleClass('is-active');
     });
 
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
 });
